e3b70cd2cc65172da3fedfbd4200435ebe51a18d
Arturo Solito IV <your-github-email@example.com>
2026-06-17 14:43:36 +0800

    feat: add serializers and basic viewsets

---
+++ rides/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+
+from .models import Ride, RideEvent, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id_user",
+            "role",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+        ]
+
+
+class RideEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RideEvent
+        fields = [
+            "id_ride_event",
+            "id_ride",
+            "description",
+            "created_at",
+        ]
+
+
+class RideSerializer(serializers.ModelSerializer):
+    id_rider = UserSerializer(read_only=True)
+    id_driver = UserSerializer(read_only=True)
+
+    id_rider_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        source="id_rider",
+        write_only=True,
+    )
+    id_driver_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        source="id_driver",
+        write_only=True,
+    )
+
+    class Meta:
+        model = Ride
+        fields = [
+            "id_ride",
+            "status",
+            "id_rider",
+            "id_driver",
+            "id_rider_id",
+            "id_driver_id",
+            "pickup_latitude",
+            "pickup_longitude",
+            "dropoff_latitude",
+            "dropoff_longitude",
+            "pickup_time",
+        ]
--- rides/views.py
+++ rides/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Ride, RideEvent, User
+from .serializers import RideEventSerializer, RideSerializer, UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all().order_by("id_user")
+    serializer_class = UserSerializer
+
+
+class RideViewSet(ModelViewSet):
+    serializer_class = RideSerializer
+
+    def get_queryset(self):
+        return (
+            Ride.objects
+            .select_related("id_rider", "id_driver")
+            .order_by("id_ride")
+        )
+
+
+class RideEventViewSet(ModelViewSet):
+    queryset = RideEvent.objects.select_related("id_ride").order_by("id_ride_event")
+    serializer_class = RideEventSerializer
